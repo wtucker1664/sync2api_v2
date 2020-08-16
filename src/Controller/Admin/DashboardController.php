1a84633a0af99f0f2b90aee8cac8b0c3d812e796
Warren Tucker <wtucker1664@me.com>
2020-08-16 13:14:56 +0100

    More changes

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use \App\Entity\User;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -27,6 +28,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToCrud('Users', 'icon class', User::class);
     }
 }
